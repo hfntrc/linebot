8fa7b51d8d3e10cc710f02b427e94e0a4594d2ec
hfntrc <fsvjdjupke@gmail.com>
2026-05-01 18:54:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def translate(text):
 """
 
     response = gemini_client.models.generate_content(
-        model="gemini-2.0-flash",
+        model="gemini-1.5-flash",
         contents=prompt,
     )
 
